bf4d74252138fd79143cd5bfe1f441b9bfc501f6
nise_nabe <nise.nabe@gmail.com>
2012-06-03 09:24:29 +0900

    fixed for coding standard

--- apps/pc_frontend/modules/member/templates/editProfileSuccess.php
+++ apps/pc_frontend/modules/member/templates/editProfileSuccess.php
@@ -3,5 +3,4 @@ $options = array(
   'title' => __('Edit Profile'),
   'url' => url_for('@member_editProfile'),
 );
-op_include_form('profileForm', array($memberForm, $profileForm), $options)
-?>
+op_include_form('profileForm', array($memberForm, $profileForm), $options);
