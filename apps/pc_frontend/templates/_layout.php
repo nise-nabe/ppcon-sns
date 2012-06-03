a9e028cf71e2fb5b4cb63cf828c79ae19105306e
nise_nabe <nise.nabe@gmail.com>
2012-06-03 13:04:39 +0900

    fixed not to display header in login page

--- apps/pc_frontend/templates/_layout.php
+++ apps/pc_frontend/templates/_layout.php
@@ -38,11 +38,13 @@ var openpne = '.$json.';
 <?php echo $op_config->get('pc_html_top') ?>
 <div id="Container" class="container" style="margin-top:60px;">
 
+<?php if (!('member' === $view->getModuleName() && 'login' === $view->getActionName())): ?>
 <div id="Header">
 <div id="HeaderContainer">
 <?php include_partial('global/header') ?>
 </div><!-- HeaderContainer -->
 </div><!-- Header -->
+<?php endif ?>
 
 <div id="Contents">
 <div id="ContentsContainer">
